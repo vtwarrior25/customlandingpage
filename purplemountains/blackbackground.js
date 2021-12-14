29bc40a88ad4bd4350f89d9a4a64e1542914f180
vtwarrior25 <vtwarrior25@gmail.com>
2021-12-14 15:59:55 -0500

    Completed both black and purple css docs
    Finished css docs for both black and purple styles, javascript to change the background color of the image not working currently. Also yet to put specific stylesheets and script files into folders

---
+++ purplemountains/blackbackground.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $("#weatherimgarea").attr("src","https://wttr.in/05767_1pQu_transparency=255_background=000000_lang=en.png");
+})
---
+++ purplemountains/purplemountains.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $("#weatherimgarea").attr("src","https://wttr.in/05767_1pQu_transparency=255_background=443D6C_lang=en.png");
+})

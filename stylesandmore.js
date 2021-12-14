9875cee3f51efa39c25fbcbbbd206475024837db
vtwarrior25 <vtwarrior25@gmail.com>
2021-12-14 15:32:53 -0500

    Removed style from html and put it into seperate css file
    Also created seperate black and purple css files for the html, but the black doesn't work right now

---
+++ stylesandmore.js
@@ -0,0 +1,24 @@
+// 1 = purple mountains, 2 = black and white
+const colorscheme = 1;
+
+
+function purple(){
+
+}
+
+
+function black(){
+    $("head").html();
+}
+
+
+
+
+
+if (colorscheme == 1){
+purple();
+} else if (colorscheme == 2){
+black();
+}
+
+
